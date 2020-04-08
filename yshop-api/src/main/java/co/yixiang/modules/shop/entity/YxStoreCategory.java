e499285532c685fc085a1a66805b88b6e31e4751
hupeng <guchengwuyue@163.com>
2020-04-08 14:43:49 +0800

    导出system_group_data

--- yshop-api/src/main/java/co/yixiang/modules/shop/entity/YxStoreCategory.java
+++ yshop-api/src/main/java/co/yixiang/modules/shop/entity/YxStoreCategory.java
@@ -18,31 +18,31 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-@ApiModel(value="YxStoreCategory对象", description="商品分类表")
+@ApiModel(value = "YxStoreCategory对象", description = "商品分类表")
 public class YxStoreCategory extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
 
-@ApiModelProperty(value = "商品分类表ID")
-@TableId(value = "id", type = IdType.AUTO)
-private Integer id;
+    @ApiModelProperty(value = "商品分类表ID")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
 
-@ApiModelProperty(value = "父id")
-private Integer pid;
+    @ApiModelProperty(value = "父id")
+    private Integer pid;
 
-@ApiModelProperty(value = "分类名称")
-private String cateName;
+    @ApiModelProperty(value = "分类名称")
+    private String cateName;
 
-@ApiModelProperty(value = "排序")
-private Integer sort;
+    @ApiModelProperty(value = "排序")
+    private Integer sort;
 
-@ApiModelProperty(value = "图标")
-private String pic;
+    @ApiModelProperty(value = "图标")
+    private String pic;
 
-@ApiModelProperty(value = "是否推荐")
-private Boolean isShow;
+    @ApiModelProperty(value = "是否推荐")
+    private Boolean isShow;
 
-@ApiModelProperty(value = "添加时间")
-private Integer addTime;
+    @ApiModelProperty(value = "添加时间")
+    private Integer addTime;
 
 }
