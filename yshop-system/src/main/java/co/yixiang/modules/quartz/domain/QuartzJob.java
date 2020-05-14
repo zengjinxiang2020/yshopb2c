c923bcff77d673d02d31449fa840965f3cf37aa8
taozi <552920369@qq.com>
2020-05-14 10:01:42 +0800

    bug--定时任务entity注解规范

--- yshop-system/src/main/java/co/yixiang/modules/quartz/domain/QuartzJob.java
+++ yshop-system/src/main/java/co/yixiang/modules/quartz/domain/QuartzJob.java
@@ -1,12 +1,9 @@
 package co.yixiang.modules.quartz.domain;
-import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
-import javax.persistence.*;
-import javax.validation.constraints.*;
 import java.sql.Timestamp;
 import java.io.Serializable;
 
@@ -14,58 +11,56 @@ import java.io.Serializable;
 * @author hupeng
 * @date 2020-05-13
 */
-@Entity
+
 @Data
-@Table(name="quartz_job")
+@TableName("quartz_job")
 public class QuartzJob implements Serializable {
 
     public static final String JOB_KEY = "JOB_KEY";
 
     /** 定时任务ID */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @TableId
     private Long id;
 
 
     /** Spring Bean名称 */
-    @Column(name = "bean_name")
+    //@Column(name = "bean_name")
     private String beanName;
 
 
     /** cron 表达式 */
-    @Column(name = "cron_expression")
+    //@Column(name = "cron_expression")
     private String cronExpression;
 
 
     /** 状态：1暂停、0启用 */
-    @Column(name = "is_pause")
+    //@Column(name = "is_pause")
     private Boolean isPause;
 
 
     /** 任务名称 */
-    @Column(name = "job_name")
+    //@Column(name = "job_name")
     private String jobName;
 
 
     /** 方法名称 */
-    @Column(name = "method_name")
+    //@Column(name = "method_name")
     private String methodName;
 
 
     /** 参数 */
-    @Column(name = "params")
+    //@Column(name = "params")
     private String params;
 
 
     /** 备注 */
-    @Column(name = "remark")
+    //@Column(name = "remark")
     private String remark;
 
 
     /** 创建日期 */
-    @Column(name = "create_time")
-    @TableField(fill= FieldFill.INSERT)
+    //@Column(name = "create_time")
+    //@TableField(fill= FieldFill.INSERT)
     private Timestamp createTime;
 
     public void copy(QuartzJob source){
--- yshop-system/src/main/java/co/yixiang/modules/quartz/domain/QuartzLog.java
+++ yshop-system/src/main/java/co/yixiang/modules/quartz/domain/QuartzLog.java
@@ -1,12 +1,10 @@
 package co.yixiang.modules.quartz.domain;
-import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
 import javax.persistence.*;
-import javax.validation.constraints.*;
 import java.sql.Timestamp;
 import java.io.Serializable;
 
@@ -14,60 +12,59 @@ import java.io.Serializable;
 * @author hupeng
 * @date 2020-05-13
 */
-@Entity
+
 @Data
-@Table(name="quartz_log")
+@TableName("quartz_log")
 public class QuartzLog implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    /** 任务日志ID */
+    @TableId
     private Long id;
 
 
     /** 任务名称 */
-    @Column(name = "baen_name")
+    //@Column(name = "baen_name")
     private String baenName;
 
 
     /** Bean名称  */
     @Column(name = "create_time")
-    @TableField(fill= FieldFill.INSERT)
+    //@TableField(fill= FieldFill.INSERT)
     private Timestamp createTime;
 
 
     /** cron表达式 */
-    @Column(name = "cron_expression")
+    //@Column(name = "cron_expression")
     private String cronExpression;
 
 
     /** 异常详细  */
-    @Column(name = "exception_detail")
+    //@Column(name = "exception_detail")
     private String exceptionDetail;
 
 
     /** 状态 */
-    @Column(name = "is_success")
+    //@Column(name = "is_success")
     private Boolean isSuccess;
 
 
     /** 任务名称 */
-    @Column(name = "job_name")
+    //@Column(name = "job_name")
     private String jobName;
 
 
     /** 方法名称 */
-    @Column(name = "method_name")
+    //@Column(name = "method_name")
     private String methodName;
 
 
     /** 参数 */
-    @Column(name = "params")
+    //@Column(name = "params")
     private String params;
 
 
     /** 耗时（毫秒） */
-    @Column(name = "time")
+    //@Column(name = "time")
     private Long time;
 
 
