507260a31f3824b654e29f03a69e70d1949510d1
xuwenbo <717567226@qq.com>
2020-05-16 12:10:32 +0800

    添加访问记录

--- yshop-system/src/main/java/co/yixiang/modules/monitor/service/impl/VisitsServiceImpl.java
+++ yshop-system/src/main/java/co/yixiang/modules/monitor/service/impl/VisitsServiceImpl.java
@@ -1,9 +1,12 @@
 package co.yixiang.modules.monitor.service.impl;
 
+import co.yixiang.common.service.impl.BaseServiceImpl;
 import co.yixiang.logging.service.mapper.LogMapper;
 import co.yixiang.modules.monitor.domain.Visits;
 import co.yixiang.modules.monitor.service.VisitsService;
+import co.yixiang.modules.monitor.service.mapper.VisitsMapper;
 import co.yixiang.utils.StringUtils;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -14,65 +17,70 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
- * @author Zheng Jie
+ * @author hupeng
  * @date 2018-12-13
  */
 @Slf4j
 @Service
 @Transactional(propagation = Propagation.SUPPORTS, readOnly = true, rollbackFor = Exception.class)
-public class VisitsServiceImpl implements VisitsService {
+public class VisitsServiceImpl extends BaseServiceImpl<VisitsMapper, Visits> implements VisitsService {
 
 
     private final LogMapper logMapper;
 
-    public VisitsServiceImpl(LogMapper logMapper) {
+    private final VisitsMapper visitsMapper;
+
+    public VisitsServiceImpl(LogMapper logMapper, VisitsMapper visitsMapper) {
         this.logMapper = logMapper;
+        this.visitsMapper = visitsMapper;
     }
 
 
     @Override
     public void save() {
         LocalDate localDate = LocalDate.now();
-//        Visits visits = logMapper.findByDate(localDate.toString());
-//        if(visits == null){
-//            visits = new Visits();
-//            visits.setWeekDay(StringUtils.getWeekDay());
-//            visits.setPvCounts(1L);
-//            visits.setIpCounts(1L);
-//            visits.setDate(localDate.toString());
-//            logMapper.insert(visits);
-//        }
+        Visits visits = this.getOne(new QueryWrapper<Visits>().lambda()
+        .eq(Visits::getDate,localDate.toString()));
+        if(visits == null){
+            visits = new Visits();
+            visits.setWeekDay(StringUtils.getWeekDay());
+            visits.setPvCounts(1L);
+            visits.setIpCounts(1L);
+            visits.setDate(localDate.toString());
+            this.save(visits);
+        }
     }
 
     @Override
     public void count(HttpServletRequest request) {
         LocalDate localDate = LocalDate.now();
-//        Visits visits = visitsRepository.findByDate(localDate.toString());
-//        visits.setPvCounts(visits.getPvCounts()+1);
-//        long ipCounts = logRepository.findIp(localDate.toString(), localDate.plusDays(1).toString());
-//        visits.setIpCounts(ipCounts);
-//        visitsRepository.save(visits);
+        Visits visits = this.getOne(new QueryWrapper<Visits>().lambda()
+                .eq(Visits::getDate,localDate.toString()));
+        visits.setPvCounts(visits.getPvCounts()+1);
+        long ipCounts = logMapper.findIp(localDate.toString(), localDate.plusDays(1).toString());
+        visits.setIpCounts(ipCounts);
+        this.save(visits);
     }
 
     @Override
     public Object get() {
         Map<String,Object> map = new HashMap<>(4);
         LocalDate localDate = LocalDate.now();
-//        Visits visits = visitsRepository.findByDate(localDate.toString());
-//        List<Visits> list = visitsRepository.findAllVisits(localDate.minusDays(6).toString(),localDate.plusDays(1).toString());
-//
-//        long recentVisits = 0, recentIp = 0;
-//        for (Visits data : list) {
-//            recentVisits += data.getPvCounts();
-//            recentIp += data.getIpCounts();
-//        }
-//        map.put("newVisits",visits.getPvCounts());
-//        map.put("newIp",visits.getIpCounts());
-//        map.put("recentVisits",recentVisits);
-//        map.put("recentIp",recentIp);
+        Visits visits = this.getOne(new QueryWrapper<Visits>().lambda()
+                .eq(Visits::getDate,localDate.toString()));
+        List<Visits> list = visitsMapper.findAllVisits(localDate.minusDays(6).toString(),localDate.plusDays(1).toString());
+
+        long recentVisits = 0, recentIp = 0;
+        for (Visits data : list) {
+            recentVisits += data.getPvCounts();
+            recentIp += data.getIpCounts();
+        }
+        map.put("newVisits",visits.getPvCounts());
+        map.put("newIp",visits.getIpCounts());
+        map.put("recentVisits",recentVisits);
+        map.put("recentIp",recentIp);
         return map;
     }
 
