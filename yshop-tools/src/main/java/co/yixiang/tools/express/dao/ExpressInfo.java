6908a1292553074c603037de9dd5c1eab296b12f
taochengbo <552920369@qq.com>
2020-05-16 16:32:35 +0800

    bug--日期修改

--- yshop-tools/src/main/java/co/yixiang/tools/express/dao/ExpressInfo.java
+++ yshop-tools/src/main/java/co/yixiang/tools/express/dao/ExpressInfo.java
@@ -2,7 +2,7 @@
  * Copyright 2018 bejson.com
  */
 /**
- * Copyright (C) 2018-2019
+ * Copyright (C) 2018-2020
  * All rights reserved, Designed By www.yixiang.co
  * 注意：
  * 本软件为www.yixiang.co开发研制，未经购买不得使用
