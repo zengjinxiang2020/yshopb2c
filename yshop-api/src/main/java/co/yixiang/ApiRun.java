01f755a05141c7415a5537cd6cd15a31ddfc80df
hupeng <guchengwuyue@163.com>
2020-05-31 11:51:02 +0800

    修改启动画面yshop标识

--- yshop-api/src/main/java/co/yixiang/ApiRun.java
+++ yshop-api/src/main/java/co/yixiang/ApiRun.java
@@ -22,6 +22,15 @@ public class ApiRun {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiRun.class, args);
+
+        System.out.println(
+                    "              __                  \n" +
+                    "  __ __ ___  / /  ___   ___       \n" +
+                    " / // /(_-< / _ \\/ _ \\ / _ \\   \n" +
+                    " \\_, //___//_//_/\\___// .__/    \n" +
+                    "/___/                /_/          \n "+
+
+                    "\n意象yshop电商系统移动端API启动成功 \n官网：https://www.yixiang.co 提供技术支持ﾞ  \n");
     }
 
     @Bean
