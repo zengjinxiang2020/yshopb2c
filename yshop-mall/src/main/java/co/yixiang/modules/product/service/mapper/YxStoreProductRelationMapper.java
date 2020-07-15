6ef5f28d303f4c83199de8f55fe1f8ebe29e9360
hupeng <guchengwuyue@163.com>
2020-07-15 16:13:47 +0800

    修正收藏sql，防止关联返回null数据

--- yshop-mall/src/main/java/co/yixiang/modules/product/service/mapper/YxStoreProductRelationMapper.java
+++ yshop-mall/src/main/java/co/yixiang/modules/product/service/mapper/YxStoreProductRelationMapper.java
@@ -33,7 +33,7 @@ public interface YxStoreProductRelationMapper extends CoreMapper<YxStoreProductR
     @Select("select B.id pid,A.category,B.store_name as storeName,B.price," +
             "B.ot_price as otPrice,B.sales,B.image,B.is_show as isShow" +
             " from yx_store_product_relation A left join yx_store_product B " +
-            "on A.product_id = B.id where A.uid=#{uid} and A.is_del = 0")
+            "on A.product_id = B.id where A.uid=#{uid} and A.is_del = 0 and B.is_del = 0")
     List<YxStoreProductRelationQueryVo> selectList(Page page, @Param("uid") Long uid);
 
 
