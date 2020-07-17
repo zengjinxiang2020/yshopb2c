4062ee9139ca3a5a05372b623142478ce8b7454e
taozi <552920369@qq.com>
2020-07-17 15:32:42 +0800

    bug--微信包名修改

--- yshop-app/src/main/java/co/yixiang/modules/wechat/rest/controller/WechatController.java
+++ yshop-app/src/main/java/co/yixiang/modules/wechat/rest/controller/WechatController.java
@@ -6,7 +6,7 @@
  * 购买后可获得全部源代码（禁止转卖、分享、上传到码云、github等开源平台）
  * 一经发现盗用、分享等行为，将追究法律责任，后果自负
  */
-package co.yixiang.modules.wechat.rest.rest;
+package co.yixiang.modules.wechat.rest.controller;
 
 import co.yixiang.annotation.AnonymousAccess;
 
