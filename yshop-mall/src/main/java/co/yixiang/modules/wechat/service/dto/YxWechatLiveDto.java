3c27658b9cc3838efe50d51b586ab3ccb0bfe511
xuwenbo <717567226@qq.com>
2020-08-31 09:23:00 +0800

    修改秒杀sql

--- yshop-mall/src/main/java/co/yixiang/modules/wechat/service/dto/YxWechatLiveDto.java
+++ yshop-mall/src/main/java/co/yixiang/modules/wechat/service/dto/YxWechatLiveDto.java
@@ -10,6 +10,7 @@ package co.yixiang.modules.wechat.service.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -27,54 +28,70 @@ public class YxWechatLiveDto implements Serializable {
     /** 直播间id */
     /** 防止精度丢失 */
     @JsonSerialize(using= ToStringSerializer.class)
+    @ApiModelProperty(value = "直播间id")
     private Long roomid;
 
     private Long id;
 
     /** 直播间标题 */
+    @ApiModelProperty(value = "直播间标题")
     private String name;
 
     /** 背景图 */
+    @ApiModelProperty(value = "背景图")
     private String coverImge;
 
     /** 分享图片 */
+    @ApiModelProperty(value = "分享图片")
     private String shareImge;
 
     /** 直播间状态 */
+    @ApiModelProperty(value = "直播间状态  101：直播中，102：未开始，103 已结束，104 禁播，105：暂停，106：异常，107：已过期")
     private Integer liveStatus;
 
     /** 开始时间 */
+    @ApiModelProperty(value = "开始时间")
     private Long startTime;
 
     /** 预计结束时间 */
+    @ApiModelProperty(value = "预计结束时间")
     private Long endTime;
 
     /** 主播昵称 */
+    @ApiModelProperty(value = "主播昵称")
     private String anchorName;
 
     /** 主播微信号 */
+    @ApiModelProperty(value = "主播微信号")
     private String anchorWechat;
 
     /** 主播头像 */
+    @ApiModelProperty(value = "主播头像")
     private String anchorImge;
 
     /** 直播间类型 1：推流 0：手机直播 */
+    @ApiModelProperty(value = "直播间类型 1：推流 0：手机直播")
     private Integer type;
 
     /** 横屏、竖屏 【1：横屏，0：竖屏】 */
+    @ApiModelProperty(value = "横屏、竖屏 【1：横屏，0：竖屏】")
     private Integer screenType;
 
     /** 是否关闭点赞 【0：开启，1：关闭】 */
+    @ApiModelProperty(value = "是否关闭点赞 【0：开启，1：关闭】")
     private Integer closeLike;
 
     /** 是否关闭评论 【0：开启，1：关闭】 */
+    @ApiModelProperty(value = "是否关闭评论 【0：开启，1：关闭】")
     private Integer closeComment;
 
     /** 是否关闭货架 【0：开启，1：关闭】 */
+    @ApiModelProperty(value = "是否关闭货架 【0：开启，1：关闭】")
     private Integer closeGoods;
     /**
      * 关联商品id多个，隔开
      */
+
     private String productId;
     private List<YxWechatLiveGoodsDto> product;
 }
