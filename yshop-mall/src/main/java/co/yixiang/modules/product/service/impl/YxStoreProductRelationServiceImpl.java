501e5373279214dfc811de6bf963121adc379f0d
LIONCITYS\lioncity <18026623439@163.com>
2020-11-27 17:01:01 +0800

    Revert "处理商品收藏"
    This reverts commit d0c551c430ee1f12614553ced49557b8de3e029f.

--- yshop-mall/src/main/java/co/yixiang/modules/product/service/impl/YxStoreProductRelationServiceImpl.java
+++ yshop-mall/src/main/java/co/yixiang/modules/product/service/impl/YxStoreProductRelationServiceImpl.java
@@ -101,7 +101,7 @@ public class YxStoreProductRelationServiceImpl extends BaseServiceImpl<YxStorePr
         YxStoreProductRelation productRelation = this.lambdaQuery()
                 .eq(YxStoreProductRelation::getProductId,productId)
                 .eq(YxStoreProductRelation::getUid,uid)
-                .eq(YxStoreProductRelation::getType,"collect")
+                .eq(YxStoreProductRelation::getType,category)
                 .one();
         if(productRelation == null) {
             throw new YshopException("已取消");
