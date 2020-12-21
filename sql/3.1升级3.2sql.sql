1195ead0554848e1a47ba6d4b5464cae440164c0
朱耘稷 <18026623439@163.com>
2020-12-21 13:59:06 +0800

    删除无用表,新增增量sql

---
+++ sql/3.1升级3.2sql.sql
@@ -0,0 +1,10 @@
+
+
+-- ----------------------------
+-- 字段修改
+-- ----------------------------
+ALTER TABLE yx_store_product ADD COLUMN is_integral  tinyint(1)   ZEROFILL NULL DEFAULT 0 COMMENT '是开启积分兑换' AFTER is_del;
+ALTER TABLE yx_store_product_attr_value ADD COLUMN integral INT(10)  DEFAULT 0 COMMENT '需要多少积分兑换' AFTER seckill_price;
+
+
+
