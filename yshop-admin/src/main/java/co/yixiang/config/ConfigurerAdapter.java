444fe6baaf05893e2f6d279dbe7bdd7e66de604a
朱耘稷 <18026623439@163.com>
2021-01-07 12:36:36 +0800

    新增docker一键部署方案

--- yshop-admin/src/main/java/co/yixiang/config/ConfigurerAdapter.java
+++ yshop-admin/src/main/java/co/yixiang/config/ConfigurerAdapter.java
@@ -43,7 +43,12 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
         config.addAllowedHeader("*");// #允许访问的头信息,*表示全部
         config.setMaxAge(18000L);// 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
         config.addAllowedMethod("OPTIONS");// 允许提交请求的方法类型，*表示全部允许
-
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
