8a2d307b2d7e7f09d7ace3e591566379dfe6ae79
taozi <552920369@qq.com>
2021-01-07 20:24:37 +0800

    修复登陆异常

--- yshop-app/src/main/java/co/yixiang/common/config/InterceptorConfig.java
+++ yshop-app/src/main/java/co/yixiang/common/config/InterceptorConfig.java
@@ -27,7 +27,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Author hupeng <610796224@qq.com>
  * @Date 2020/4/30
  **/
-@Configuration(proxyBeanMethods = false)
+@Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
     @Value("${file.path}")
